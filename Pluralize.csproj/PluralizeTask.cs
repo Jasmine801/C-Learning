cefc9c10afdd7e37d2448e0ed4d7a986e9a59b2e
Jasmine801 <71119129+Jasmine801@users.noreply.github.com>
2022-05-27 16:25:36 +0300

    Add files via upload

---
+++ Pluralize.csproj/PluralizeTask.cs
@@ -0,0 +1,41 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+			if (count < 100)
+			{
+				if (count == 11 || count == 12 || count == 13 || count == 14)
+				{
+					return "рублей";
+				}
+				else if (count % 10 == 1)
+					return "рубль";
+				else if (count % 10 == 2 || count % 10 == 3 || count % 10 == 4)
+				{
+					return "рубля";
+				}
+				else
+					return "рублей";
+			}
+			else
+			{
+				count = count % 100;
+				if (count == 11 || count == 12 || count == 13 || count == 14)
+				{
+					return "рублей";
+				}
+				else if (count % 10 == 1)
+					return "рубль";
+				else if (count % 10 == 2 || count % 10 == 3 || count % 10 == 4)
+				{
+					return "рубля";
+				}
+				else
+					return "рублей";
+			}
+		}
+	}
+}
